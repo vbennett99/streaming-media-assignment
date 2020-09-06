0490bc72a89c38d727dc790687bd33cee6b70074
Vincent Bennett <vmbennett99@gmail.com>
2020-09-06 16:17:03 -0400

    split up the loadFile functions into smaller more digestable functions

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -1,7 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 
-const loadFile = (request, response, filePath, contentType) => {
+function writeResponseHead(response, start, end, total, chunksize, contentType) {
+  response.writeHead(206, {
+    'Content-Range': `bytes ${start}-${end}/${total}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': chunksize,
+    'Content-Type': contentType,
+  });
+}
+
+const createStream = (response, file, start, end) => {
+  const stream = fs.createReadStream(file, { start, end });
+  stream.on('open', () => {
+    stream.pipe(response);
+  });
+
+  stream.on('error', (streamErr) => {
+    response.end(streamErr);
+  });
+  return stream;
+};
+
+const getBytePositions = (request) => {
+  let { range } = request.headers;
+
+  if (!range) {
+    range = 'bytes=0-';
+  }
+
+  return range.replace(/bytes=/, '').split('-');
+};
+
+function loadFile(request, response, filePath, contentType) {
   const file = path.resolve(__dirname, filePath);
 
   fs.stat(file, (err, stats) => {
@@ -12,16 +43,9 @@ const loadFile = (request, response, filePath, contentType) => {
       return response.end(err);
     }
 
-    let { range } = request.headers;
-
-    if (!range) {
-      range = 'bytes=0-';
-    }
-
-    const positions = range.replace(/bytes=/, '').split('-');
+    const positions = getBytePositions(request);
 
     let start = parseInt(positions[0], 10);
-
     const total = stats.size;
     const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
 
@@ -31,26 +55,12 @@ const loadFile = (request, response, filePath, contentType) => {
 
     const chunksize = (end - start) + 1;
 
-    response.writeHead(206, {
-      'Content-Range': `bytes ${start}-${end}/${total}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': contentType,
+    writeResponseHead(response, start, end, total, chunksize, contentType);
 
-    });
-
-    const stream = fs.createReadStream(file, { start, end });
-    stream.on('open', () => {
-      stream.pipe(response);
-    });
-
-    stream.on('error', (streamErr) => {
-      response.end(streamErr);
-    });
-
+    const stream = createStream(response, file, start, end);
     return stream;
   });
-};
+}
 
 const getParty = (request, response) => {
   loadFile(request, response, '../client/party.mp4', 'video/mp4');
